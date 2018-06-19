1ec73de0f8be6fd2cf27e5ea8bf529d965030c43
shreshtamanohar <40389960+shreshtamanohar@users.noreply.github.com>
2018-06-19 22:22:12 +0530

    Update Hero.js

--- src/js/components/Hero.js
+++ src/js/components/Hero.js
@@ -3,7 +3,7 @@ import { addClass, div, img, p, section, text } from '../builders';
 export default function hero() {
   const logo = addClass(img('static/fancybear_white.png'), 'logo');
   const the = addClass(p(text('The')), 'hero-text-light');
-  const fancyBear = addClass(p(text('Fancy Bear')), 'hero-text-bold');
+  const fancyBear = addClass(p(text('Kudla Bear')), 'hero-text-bold');
   const eateries = addClass(p(text('Eateries')), 'hero-text-light');
 
   const container = addClass(div(logo, the, fancyBear, eateries), 'container');
